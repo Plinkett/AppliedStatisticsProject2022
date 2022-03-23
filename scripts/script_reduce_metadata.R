436d107179a78737a0ab435d8aafb8177ee15bdf
Plinkett <carlos.santillan.vasquez@gmail.com>
2022-03-23 22:34:35 +0100

    updating cleaning script
    updates the cleaning script

--- scripts/script_reduce_metadata.R
+++ scripts/script_reduce_metadata.R
@@ -25,5 +25,30 @@ sum(covid$pango_lineage == "unclassifiable")
 covid <- subset(covid, covid$pango_lineage != "unclassifiable")
 nrow(covid)
 
+# Deleting useless and problematic rows
+covid <- subset(covid, covid$date != "?")
+covid <- subset(covid, covid$host != "")
+covid <- subset(covid, covid$Nextstrain_clade != "")
+covid <- subset(covid, covid$pango_lineage != "")
+
+# They are all humans anyway
+covid <- covid[,-6]
+
+# Creating covid_animals dataset
+covid_animals <- subset(covid, covid$host != "Homo sapiens")
+
+# See unique parameters
+
+lineages <- unique(covid$pango_lineage)
+lineages <- as.data.frame(lineages)
+clades <- as.data.frame(unique(covid$Nextstrain_clade))
+region_country_division <- as.data.frame(unique(covid[,c('region','country','division')]))
+
 # Togliete l'hashtag del comando sotto per salvare il dataframe in un file .csv 
-# write.csv(data.frame(covid), file = "new_metadata.csv",row.names=TRUE)
+# write.csv(data.frame(covid), file = "metadata_filtered_humans.csv",row.names=FALSE)
+# write.csv(data.frame(covid_animals), file = "metadata_animals.csv",row.names=FALSE)
+# write.csv(data.frame(lineages), file = "lineages.csv",row.names=FALSE)
+# write.csv(data.frame(clades), file = "clades.csv",row.names=FALSE)
+# write.csv(data.frame(region_country_division), file = "region_country_division.csv",row.names=FALSE)
+
+
